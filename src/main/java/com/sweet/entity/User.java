5ff6e485cb78f1014fd666182b3e2d0e00ba0bdc
bryan <brayanby2002@gmail.com>
2022-11-12 13:56:18 -0500

    ordenar codigo

--- src/main/java/com/sweet/entity/User.java
+++ src/main/java/com/sweet/entity/User.java
@@ -1,10 +1,10 @@
 package com.sweet.entity;
 
 	import java.util.Collection;
-import java.util.HashSet;
-import java.util.Set;
-
-import javax.persistence.Column;
+	import java.util.HashSet;
+	import java.util.Set;
+	
+	import javax.persistence.Column;
 	import javax.persistence.Entity;
 	import javax.persistence.FetchType;
 	import javax.persistence.GeneratedValue;
@@ -14,10 +14,10 @@ import javax.persistence.Column;
 	import javax.persistence.ManyToOne;
 	import javax.persistence.Table;
 
-import org.springframework.security.core.GrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-
-import com.sweet.utils.Authority;
+	import org.springframework.security.core.GrantedAuthority;
+	import org.springframework.security.core.userdetails.UserDetails;
+	
+	import com.sweet.utils.Authority;
 
 @Entity
 @Table(name = "tb_user")
